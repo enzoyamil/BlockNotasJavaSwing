76b22eedd451d5d6bb84477e889109272f13dfb6
Enzo <enzoyamil1964@gmail.com>
2018-02-14 19:44:08 -0400

    Notas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-Ventana x=new Ventana();
-x.setSize(700,600);
-x.setVisible(true);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ventana x = new Ventana();
+                x.setSize(700, 600);
+                x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                x.setVisible(true);
+            }
+        });
+
     }
 }
--- src/com/company/Ventana.java
+++ src/com/company/Ventana.java
@@ -1,6 +1,5 @@
 package com.company;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -81,6 +80,7 @@ public class Ventana extends JFrame{
       menu[4].add(item[22]);
       menu[4].add(item[23]);
       add(texto);
+      System.out.println("esto se muestra");
       item[7].addActionListener(new ActionListener() {
           @Override
           public void actionPerformed(ActionEvent e) {
